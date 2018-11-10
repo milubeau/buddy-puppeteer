f68786d803553893b495cedd948191e2eea9d3f9
milubeau <44909115+milubeau@users.noreply.github.com>
2018-11-10 15:10:35 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://buddy.works');
+  await page.goto('https://milubeau.github.io/trang/chaych2.html');
   await page.screenshot({path: 'buddy-screenshot.png'});
   console.log(await page.content()); 
   setTimeout(async () => await await page.close(), 3600000);
